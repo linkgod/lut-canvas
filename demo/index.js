e7d6b54f04aed024c8759f0f97c02f571428c65e
皓勋 <wangxun.wx@alibaba-inc.com>
2020-06-30 20:47:03 +0800

    move demo files to demo dir

---
+++ demo/index.js
@@ -0,0 +1,13 @@
+import './styles.css';
+import lut from '../src/index';
+
+lut();
+
+document.getElementById('app').innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+export default function lut() {
+  console.log('load src/index.js success')
+}
